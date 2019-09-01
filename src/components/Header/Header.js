54ad056ee8924c0f45cdf3d07ce34deb3633310c
05K4R1N <oskr.cbba2010@gmail.com>
2019-09-01 19:23:45 -0400

    [D] Removing materalize and adding material UI

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import Logo from './Logo/Logo';
 import NavigationItems from './NavigationItems/NavigationItems';
-
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import classes from './Header.module.css';
 const header = (props) => (
-    <nav>
-        <div class="nav-wrapper">
-            <Logo/>
-            <NavigationItems/>
-        </div>
-    </nav>
+    <AppBar position="static">
+        <Toolbar className = {classes.Header} variant="dense">
+            <IconButton edge="start"  color="inherit" aria-label="menu">
+            <MenuIcon />
+            </IconButton>
+            <ul>
+                <li><a href="#">Link 1</a></li>
+                <li><a href="#">Link 2</a></li>
+                <li><a href="#">Link 3</a></li>
+            </ul>
+        </Toolbar>
+    </AppBar>
 );
 export default header;

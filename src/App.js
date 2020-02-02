7937b6b71a9f60e3c65a800b46895615ea1af9f6
05K4R1N <oskr.cbba2010@gmail.com>
2020-02-02 18:39:31 -0400

    [A] Preparing Axios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
 import './App.css';
-
+//import axios from './axios-weather';
 class App extends Component{
   render(){
+    /*const API_KEY = "066b6316792b3e2fc7e8cfcadb74eb81";
+    axios.get('?q=Manchester,uk&appid='+API_KEY).then(
+      res=> {
+          console.log(res);
+      }).catch(err => {
+        console.log(err);
+    });*/
     return(
-      <div>
-        <h1>test</h1>
+      <div> 
       </div>
     )
   }

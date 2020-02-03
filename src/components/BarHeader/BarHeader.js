81db1bcd9e16dc024e0639fac35b2016c3686dc5
05K4R1N <oskr.cbba2010@gmail.com>
2020-02-03 07:40:12 -0400

    [D] Removing makeStyles, adding module styles

--- src/components/BarHeader/BarHeader.js
+++ src/components/BarHeader/BarHeader.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -8,34 +7,24 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
-const useStyles = makeStyles(theme => ({
-    root: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
-  }));
+import classes from './BarHeader.module.css';
 
-  export default function ButtonAppBar() {
-    const classes = useStyles();
-  
+
+const barHeader = () => {
     return (
-      <div className={classes.root}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" className={classes.title}>
-              News
-            </Typography>
-            <Button color="inherit">Login</Button>
-          </Toolbar>
-        </AppBar>
-      </div>
+        <div className={classes.Root}>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton edge="start" className={classes.MenuButton} color="inherit" aria-label="menu">
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" className={classes.Title}>
+                News
+              </Typography>
+              <Button color="inherit">Login</Button>
+            </Toolbar>
+          </AppBar>
+        </div>
     );
-  }
+}
+export default barHeader;

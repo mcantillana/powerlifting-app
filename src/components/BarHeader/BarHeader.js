54403526f4015151fb49ed58ee194ffdc639648f
05K4R1N <oskr.cbba2010@gmail.com>
2020-02-04 08:06:44 -0400

    [A] Sidedrawer development

--- src/components/BarHeader/BarHeader.js
+++ src/components/BarHeader/BarHeader.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{Component} from 'react';
 
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -6,25 +6,90 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import Drawer from '@material-ui/core/Drawer';
+import clsx from 'clsx';
 
 import classes from './BarHeader.module.css';
 
 
-const barHeader = () => {
-    return (
-        <div className={classes.Root}>
-          <AppBar position="static">
-            <Toolbar>
-              <IconButton edge="start" className={classes.MenuButton} color="inherit" aria-label="menu">
-                <MenuIcon />
-              </IconButton>
-              <Typography variant="h6" className={classes.Title}>
-                News
-              </Typography>
-              <Button color="inherit">Login</Button>
-            </Toolbar>
-          </AppBar>
-        </div>
-    );
+class BarHeader extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            open: false,
+            theme: {
+                direction: ''
+            }
+        }
+    }
+    handleDrawerOpen = () => {
+        this.setState({open: true});
+    };
+    handleDrawerClose = () => {
+        this.setState({open: false});
+      };
+    render(){
+        return (
+            <div className={classes.BarHeader}>
+                <AppBar position="static">
+                    <Toolbar>
+                    <IconButton edge="start" 
+                        className={clsx(classes.menuButton, this.state.open && classes.hide)} 
+                        color="inherit" 
+                        aria-label="menu"
+                        onClick={this.handleDrawerOpen}  >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" className={classes.Title}>
+                        News
+                    </Typography>
+                    <Button color="inherit">Login</Button>
+                    </Toolbar>
+                </AppBar>
+                <Drawer
+                    className={classes.drawer}
+                    variant="persistent"
+                    anchor="left"
+                    open={this.state.open}
+                    classes={{
+                    paper: classes.drawerPaper,
+                    }}
+                >
+                    <div className={classes.drawerHeader}>
+                        <IconButton onClick={this.handleDrawerClose}>
+                            {this.state.theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                        </IconButton>
+                    </div>
+                    <Divider />
+                    <List>
+                    {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                        <ListItem button key={text}>
+                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                        <ListItemText primary={text} />
+                        </ListItem>
+                    ))}
+                    </List>
+                    <Divider />
+                    <List>
+                    {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                        <ListItem button key={text}>
+                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                        <ListItemText primary={text} />
+                        </ListItem>
+                    ))}
+                    </List>
+                </Drawer>
+            </div>
+        )
+    }
 }
-export default barHeader;
+export default BarHeader;

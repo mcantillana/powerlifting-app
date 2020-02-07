9f809299c17851b00db974eb9a4ddf2b1d757c39
05K4R1N <oskr.cbba2010@gmail.com>
2020-02-07 08:11:32 -0400

    [A] Starting with component bulder

--- src/containers/AhorroBuilder/AhorroBuilder.js
+++ src/containers/AhorroBuilder/AhorroBuilder.js
@@ -1,12 +1,25 @@
 import React, {Component} from 'react';
+import Container from '@material-ui/core/Container';
 
 class AhorroBuilder extends Component{
     constructor(props){
         super(props);
+
+        this.state = {
+            ahorro: 0,
+            numeroCuenta: 0
+        };
     }
     render(){
         return(
-            <h1>Ahorro Builder</h1>
+            <div>
+                <Container maxWidth="lg">
+                    <div>
+                        <p>Depositado: x Bs.</p>
+                        <p>Numero de Cuenta: XXXXXXX</p>
+                    </div>
+                </Container>
+            </div>
         )
     }
 }

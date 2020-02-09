896a0b84d5c36556252e3775a34c05c860cc9e9f
05K4R1N <oskr.cbba2010@gmail.com>
2020-02-09 11:10:11 -0400

    [D|M] Removing items from drawer, and replacing with two main

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -4,12 +4,12 @@ import IconButton from '@material-ui/core/IconButton';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
 import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import Drawer from '@material-ui/core/Drawer';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import MonetizationOnIcon from '@material-ui/icons/MonetizationOn';
 
 import classes from './SideDrawer.module.css';
 
@@ -31,21 +31,14 @@ const sideDrawer = (props) => {
             </div>
             <Divider />
             <List>
-            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                <ListItem button key={text}>
-                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                <ListItemText primary={text} />
+                <ListItem button key="Perfil">
+                    <ListItemIcon><AccountCircleIcon/></ListItemIcon>
+                    <ListItemText primary="Mi Perfil"/>
                 </ListItem>
-            ))}
-            </List>
-            <Divider />
-            <List>
-            {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                <ListItem button key={text}>
-                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                <ListItemText primary={text} />
+                <ListItem button key="Efectivo">
+                    <ListItemIcon><MonetizationOnIcon/></ListItemIcon>
+                    <ListItemText primary="Movimientos"/>
                 </ListItem>
-            ))}
             </List>
         </Drawer>
     );

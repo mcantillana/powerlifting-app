8af00aba4e87db98a66122d9df09b179deec2f0e
05K4R1N <oskr.cbba2010@gmail.com>
2020-02-10 23:57:49 -0400

    [A] Adding styles for Profile and removing old styles

--- src/components/User/Profile/Profile.js
+++ src/components/User/Profile/Profile.js
@@ -1,20 +1,38 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+
+import classes from './Profile.module.css';
 
 const profile = () => {
     return (
-       <div>
-           <div>
-                <strong>Apellidos:</strong>
-                <p></p>
-           </div>
-           <div>
-               <strong>Nombres:</strong>
-               <p></p>
-           </div>
-           <div>
-               <strong>Nro. Documento:</strong>
-               <p></p>
-           </div>
+       <div className={classes.Profile}>
+           <Grid container
+                spacing={0}
+                direction="column"
+                alignItems="center"
+                justify="center">
+                <div className={classes.ProfileInfo}>
+                    <Grid container className={classes.ProfileData}
+                            xs={12} 
+                            sm={6}
+                            >
+                            <strong>Apellidos:</strong>
+                            <p>XXXXXXXXXXXXXXXXXXX</p>
+                    </Grid>
+                    <Grid container className={classes.ProfileData}
+                            xs={12} 
+                            sm={6}>
+                        <strong>Nombres:</strong>
+                        <p>XXXXXXXXXXXXXXX</p>
+                    </Grid>
+                    <Grid container className={classes.ProfileData}
+                            xs={12} 
+                            sm={6}>
+                        <strong>Nro. Documento:</strong>
+                        <p>XXXXXXXXXXXXX</p>
+                    </Grid>
+                </div>
+            </Grid>
        </div>
     )
 }

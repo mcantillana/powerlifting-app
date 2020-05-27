73f79208aef860978db0aa4cbedc0391e5ff354a
05K4R1N <oskr.cbba2010@gmail.com>
2020-05-26 22:38:20 -0400

    [M] Fixing issue with styles

--- src/components/User/Information/Data/Data.js
+++ src/components/User/Information/Data/Data.js
@@ -3,11 +3,13 @@ import Grid from '@material-ui/core/Grid';
 
 import classes from './Data.module.css';
 const data = (props) => {
+    let subtitle = props.subtitle;
+    let subtitleCapitalized = subtitle.charAt(0).toUpperCase() + subtitle.slice(1);
     return (
         <Grid item container className={classes.Data}
                 xs={12} 
                 sm={6}>
-                <strong>{props.subtitle}:</strong>
+                <strong>{subtitleCapitalized}:</strong>
                 <p>{props.value}</p>
         </Grid>
     );
--- src/components/User/Information/Information.js
+++ src/components/User/Information/Information.js
@@ -5,10 +5,7 @@ import classes from './Information.module.css';
 
 const profileData = (props) => {
     const information = props.info.profile;
-    console.log(information)
     let dataInformation = Object.keys(information).map(function(key) {
-        /*console.log(key);
-        console.log(information[key]) */
         return <Data key = {key} subtitle = {key} value = {information[key]} />;
       })
     /*let subtitle = props.dataName;

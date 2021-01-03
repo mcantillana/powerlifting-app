abb0f59af5b41b5fd7167a81e3dd5f5b78921d3a
05K4R1N <oskr.cbba2010@gmail.com>
2021-01-03 12:08:35 -0400

    [M] Adding material-ui react dependency menu-icon

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,18 @@
-import NavigationItems from '../NavigationItems/NavigationItems';
-import classes from './Toolbar.module.css';
+import AppBar from '@material-ui/core/AppBar';
+
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 const toolbar = (props) => {
     return (
-       <header className={classes.Toolbar}>
-           <nav>
-                <NavigationItems/>
-           </nav>
-       </header>
-    )
+        <AppBar position="static">
+            <Toolbar>
+                <IconButton edge="start" color="inherit" aria-label="menu">
+                    <MenuIcon />
+                </IconButton>
+            </Toolbar>
+        </AppBar>
+    );
 };
 export default toolbar;

b28f2efc50201e912c4e74f87128f44757fc8001
05K4R1N <oskr.cbba2010@gmail.com>
2021-03-29 08:23:10 -0400

    [M] MODIFYING table

--- src/components/History/History.js
+++ src/components/History/History.js
@@ -19,9 +19,9 @@ import FilterListIcon from '@material-ui/icons/FilterList';
 import { lighten } from '@material-ui/core/styles/colorManipulator';
 
 let counter = 0;
-function createData(name, fat, carbs, protein) {
+function createData(movement, weight) {
   counter += 1;
-  return { id: counter, name, fat, carbs, protein };
+  return { id: counter, movement, weight };
 }
 
 function desc(a, b, orderBy) {
@@ -49,10 +49,8 @@ function getSorting(order, orderBy) {
 }
 
 const rows = [
-  { id: 'name', numeric: false, disablePadding: true, label: 'Dessert (100g serving)' },
-  { id: 'fat', numeric: true, disablePadding: false, label: 'Fat (g)' },
-  { id: 'carbs', numeric: true, disablePadding: false, label: 'Carbs (g)' },
-  { id: 'protein', numeric: true, disablePadding: false, label: 'Protein (g)' },
+  { id: 'movement', numeric: false, disablePadding: true, label: 'Movimiento' },
+  { id: 'weight', numeric: true, disablePadding: false, label: 'Peso (Kg.)' },
 ];
 
 class EnhancedTableHead extends React.Component {
@@ -197,22 +195,22 @@ const styles = theme => ({
 class History extends React.Component {
   state = {
     order: 'asc',
-    orderBy: 'fat',
+    orderBy: 'weight',
     selected: [],
     data: [
-      createData('Cupcake', 305, 3.7, 67, 4.3),
-      createData('Donut', 452, 25.0, 51, 4.9),
-      createData('Eclair', 262, 16.0, 24, 6.0),
-      createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-      createData('Gingerbread', 356, 16.0, 49, 3.9),
-      createData('Honeycomb', 408, 3.2, 87, 6.5),
-      createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-      createData('Jelly Bean', 375, 0.0, 94, 0.0),
-      createData('KitKat', 518, 26.0, 65, 7.0),
-      createData('Lollipop', 392, 0.2, 98, 0.0),
-      createData('Marshmallow', 318, 0, 81, 2.0),
-      createData('Nougat', 360, 19.0, 9, 37.0),
-      createData('Oreo', 437, 18.0, 63, 4.0),
+      createData('Cupcake', 305),
+      createData('Donut', 452),
+      createData('Eclair', 262),
+      createData('yoghurt', 159),
+      createData('Gingerbread', 356),
+      createData('Honeycomb', 408),
+      createData('Ice cream sandwich', 237),
+      createData('Jelly Bean', 375),
+      createData('KitKat', 518),
+      createData('Lollipop', 392),
+      createData('Marshmallow', 318),
+      createData('Nougat', 360),
+      createData('Oreo', 437),
     ],
     page: 0,
     rowsPerPage: 5,
@@ -305,11 +303,9 @@ class History extends React.Component {
                         <Checkbox checked={isSelected} />
                       </TableCell>
                       <TableCell component="th" scope="row" padding="none">
-                        {n.name}
+                        {n.movement}
                       </TableCell>
-                      <TableCell align="right">{n.fat}</TableCell>
-                      <TableCell align="right">{n.carbs}</TableCell>
-                      <TableCell align="right">{n.protein}</TableCell>
+                      <TableCell align="right">{n.weight}</TableCell>
                     </TableRow>
                   );
                 })}

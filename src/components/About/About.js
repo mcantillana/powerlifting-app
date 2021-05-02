7027f58da856d08742c412be0c6cf2f4c16b7b82
05K4R1N <oskr.cbba2010@gmail.com>
2021-05-01 23:24:00 -0400

    [A] Working on about page (Starting)

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,5 +1,18 @@
 const About = (props) => {
-    return (<h1>test</h1>);
+    return(
+        <div>
+            <div>Aca va una imagen</div>
+            <div>
+                Esta es una web app hecha con React.js cuyo objetivo es poder 
+                hacer registro de las marcas en cada uno de los movimientos en 
+                la disciplina Powerlifting y obtener las marcas maximas de levantamiento 
+                que se obtuvo.
+
+                Este proyecto fue hecho por el Ing. Oscar Rolando Gamboa A. con el objetivo 
+                de aplicar conocimientos adquiridos en React.js.
+            </div>
+        </div>
+    )
 };
 
 export default About;

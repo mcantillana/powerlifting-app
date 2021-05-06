4456bae183ff1df14f6fcfbd128d3f7841979262
05K4R1N <oskr.cbba2010@gmail.com>
2021-05-06 08:23:37 -0400

    [M] Replacing template new

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,67 +1,92 @@
 import React from 'react';
-import image1 from '../../assets/images/power_1.png';
-import image2 from '../../assets/images/power_2.png';
-import image3 from '../../assets/images/power_3.png';
+import Grid from '@material-ui/core/Grid';
+
 import classes from './About.module.css';
 
 const About = (props) => {
     return(
-        <section className={classes.WhyUs} data-section="section2">
+        <div>
+        <div class="page-heading header-text">
             <div class="container">
                 <div class="row">
-                    <div class="col-md-12">
-                    <div className={classes.sectionHeading}>
-                        <h2>Why choose Grad School?</h2>
-                    </div>
-                    </div>
-                    <div class="col-md-12">
-                    <div id={classes.tabs}>
-                        <ul>
-                        <li><a href='#tabs-1'>Best Education</a></li>
-                        <li><a href='#tabs-2'>Top Management</a></li>
-                        <li><a href='#tabs-3'>Quality Meeting</a></li>
-                        </ul>
-                        <section className={classes.tabsContent}>
-                            <article id='tabs-1'>
-                                <div class="row">
-                                <div class="col-md-6">
-                                    <img src={image1} alt=""/>
-                                </div>
-                                <div class="col-md-6">
-                                    <h4>Best Education</h4>
-                                    <p>Grad School is free educational HTML template with Bootstrap 4.5.2 CSS layout. Feel free to use it for educational or commercial purposes. You may want to make <a href="https://paypal.me/templatemo" target="_parent" rel="sponsored">a little donation</a> to TemplateMo. Please tell your friends about us. Thank you.</p>
-                                </div>
-                                </div>
-                            </article>
-                            <article id='tabs-2'>
-                                <div class="row">
-                                <div class="col-md-6">
-                                    <img src={image2} alt=""/>
-                                </div>
-                                <div class="col-md-6">
-                                    <h4>Top Level</h4>
-                                    <p>You can modify this HTML layout by editing contents and adding more pages as you needed. Since this template has options to add dropdown menus, you can put many HTML pages.</p> 
-                                    <p>Suspendisse tincidunt, magna ut finibus rutrum, libero dolor euismod odio, nec interdum quam felis non ante.</p>
-                                </div>
-                                </div>
-                            </article>
-                            <article id='tabs-3'>
-                                <div class="row">
-                                <div class="col-md-6">
-                                    <img src={image3} alt=""/>
-                                </div>
-                                <div class="col-md-6">
-                                    <h4>Quality Meeting</h4>
-                                    <p>You are NOT allowed to redistribute this template ZIP file on any template collection website. However, you can use this template to convert into a specific theme for any kind of CMS platform such as WordPress. For more information, you shall <a rel="nofollow" href="https://templatemo.com/contact" target="_parent">contact TemplateMo</a> now.</p>
-                                </div>
-                                </div>
-                            </article>
-                        </section>
-                    </div>
-                    </div>
+                <div class="col-md-12">
+                    <h1>About Us</h1>
+                    <span>We have over 20 years of experience</span>
+                </div>
                 </div>
             </div>
-        </section>
+        </div>
+
+    <div class="more-info about-info">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="more-info-content">
+              <div class="row">
+                <div class="col-md-6 align-self-center">
+                  <div class="right-content">
+                    <span>our solid background on finance</span>
+                    <h2>Get to know about <em>our company</em></h2>
+                    <p>Fusce nec ultrices lectus. Duis nec scelerisque risus. Ut id tempor turpis, ac dignissim ipsum. Nulla ullamcorper, ipsum vel condimentum congue, mi odio vehicula tellus, sit amet malesuada justo sem. 
+                    <br/><br/>Pellentesque in sagittis lacus, vel auctor sem. Quisque eu quam eleifend, ullamcorper dui nec, luctus quam.</p>
+                    <a href="" class="filled-button">Read More</a>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="left-image">
+                    <img src="assets/images/about-image.jpg" alt=""/>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="team">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="section-heading">
+              <h2>Our team <em>members</em></h2>
+              <span>Suspendisse a ante in neque iaculis lacinia</span>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="team-item">
+              <img src="assets/images/team_01.jpg" alt=""/>
+              <div class="down-content">
+                <h4>William Smith</h4>
+                <span>Co-Founder</span>
+                <p>In sem sem, dapibus non lacus auctor, ornare sollicitudin lacus. Aliquam ipsum urna, semper quis.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="team-item">
+              <img src="assets/images/team_02.jpg" alt=""/>
+              <div class="down-content">
+                <h4>Mary Houston</h4>
+                <span>Chief Marketing Officer</span>
+                <p>In sem sem, dapibus non lacus auctor, ornare sollicitudin lacus. Aliquam ipsum urna, semper quis.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="team-item">
+              <img src="assets/images/team_03.jpg" alt=""/>
+              <div class="down-content">
+                <h4>Paul Walker</h4>
+                <span>Financial Analyst</span>
+                <p>In sem sem, dapibus non lacus auctor, ornare sollicitudin lacus. Aliquam ipsum urna, semper quis.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
     );
 };
 
